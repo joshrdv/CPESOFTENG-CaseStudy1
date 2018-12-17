0d62ea4ce80188371a4a521caeef7a76c200c554
Maria Emreen Juarez Viray <emreenviray@gmail.com>
2018-12-17 23:18:52 +0800

    Msystem-v4

--- Msystem-v1.cpp
+++ Msystem-v1.cpp
@@ -1,72 +1,135 @@
-//management system v1.1
 #include <iostream>
+#include <stdlib.h>
 #include <fstream>
-
 using namespace std;
 
-int main(){
-	int choice;
-	char projectChoice;
-	
-	int id(0),pages(50),priority;
+struct node {
+	int id, pages, priority;
 	char title[100];
+	struct node * next;
+} *head;
+
+//INSERT FUNCTIONS HERE!!!
+void input_project(){
+	system("cls");
+	cout << "ENTER A PROJECT" << endl;
 	
-	ofstream myfile("inputs.txt",ios::app);
+	fstream myfile;
+	int id(1),pages(50),priority;
+	char c,title[100];
 	
-	cout<<"Main menu: "<<endl;
-	cout<<"[1] Input project details"<<endl;
-	cout<<"[2] View projects"<<endl;
-	cout<<"[3] Exit"<<endl;
-	cout<<"Enter option: ";
-	cin>>choice;
-	
-	if(choice==1){
+	myfile.open("inputs.txt");
+   	while (myfile) {
+    	while (myfile && c != '\n') {
+       		myfile.get(c);
+     	}
 		id++;
-		myfile<<id<<"\t";
-		cout<<"Enter title of the manuscript: "; //Title
-		cin>>title; 
-		myfile<<title<<"\t";
-		//cin.getline(title,100);
-		do{
-			cout<<"Enter number of pages: "; //Pages
-			cin>>pages;
-			myfile<<pages<<"\t";
-			if(pages<50||pages>500)
-				cout<<"The pages of the manuscript exceeds the given range."<<endl;
-		}while((pages<50)||(pages>500));
-		cout<<"Enter the priority of the manuscript: "; //Priority
-		cin>>priority;
-		myfile<<priority<<"\n";
-		myfile.close();
-		cout<<"\nPlacing inputs done successfully!";
+		myfile.get(c);
+   	}
+   	myfile.close();
+	myfile.open("inputs.txt",ios::app);
+	cout<<"Enter title of the manuscript: "; //Title
+	cin>>title;
+	while((pages>=50)||(pages<=500)){
+		cout<<"Enter number of pages: "; //Pages
+		cin>>pages;
+		if(pages<50||pages>500) cout<<"The pages of the manuscript exceeds the given range."<<endl;
+		else break;
 	}
-	else if(choice==2){
-		cout<<"\t[a] One project"<<endl;
-		cout<<"\t[b] Completed projects "<<endl;
-		cout<<"\t[c] All projects"<<endl;
-		cout<<"\tEnter option: ";
-		cin>>projectChoice;
-		if(projectChoice=='a'){
-			system("cls");
-			cout<<"a";
-		}
-		else if(projectChoice=='b'){
-			system("cls");
-			cout<<"b";
-		}
-		else if(projectChoice=='c'){
-			system("cls");
-			cout<<"c";
-		}
-		else{
-			cout<<"Invalid choice!";
-		}
-	}
-	else if(choice==3){
-		;
-	}
-	else{
-		cout<<"Invalid choice!";
-	}
+	cout<<"Enter the priority of the manuscript: "; //Priority
+	cin>>priority;
+	myfile<<id<<"\t"<<title<<"\t"<<pages<<"\t"<<priority<<"\n";
+	myfile.close();
 	
+	cout<<"ID"<<"\t"<<"TITLE"<<"\t"<<"PAGES"<<"\t"<<"PRIORITY"<<"\n";
+	cout<<id<<"\t"<<title<<"\t"<<pages<<"\t"<<priority<<"\n";
+	cout<<"\nPlacing inputs done successfully!\n";
+}
+
+void view_project_a(){
+	system("cls");
+
+	int id,pages,priority,searchId;
+	char title[100];
+	bool buff=true;
+
+	cout<<"Search for one project"<<endl;
+	cout<<"Enter the ID for the manuscript: ";
+	cin>>searchId;
+	
+	ifstream infile("inputs.txt");
+	
+	while (infile>>id>>title>>pages>>priority){
+		if(searchId==id){
+			buff=false;
+			break;
+		}
+	}
+	if(!buff) cout<<"ID: "<<id<<"\tTITLE: "<<title<<"\tPAGES: "<<pages<<"\tPRIORITY: "<<priority<<endl;
+	else cout<<"NOT FOUND!";
+	infile.close();
+}
+
+int main(){
+	char choice;
+	bool buff=true;
+	while(1){
+		system("cls");
+		cout<<"Main menu: "<<endl;
+		cout<<"[1] Input Project Details"<<endl;
+		cout<<"[2] View Projects"<<endl;
+		cout<<"[3] Schedule Projects"<<endl;
+		cout<<"[4] Get a Project"<<endl;
+		cout<<"[5] Exit"<<endl;
+		cout<<"Enter option: ";
+		cin>>choice;
+		switch(choice){
+			case '1': input_project(); break;
+			case '2': 
+				while(buff){
+					system("cls");
+					cout<<"[a] One Project"<<endl;
+					cout<<"[b] Completed Projects"<<endl;
+					cout<<"[c] All Projects"<<endl;
+					cout<<"[x] Back"<<endl;
+					cout<<"Enter option: ";
+					cin>>choice;
+					switch(choice){
+						case 'A':
+						case 'a': view_project_a(); buff=false; break;
+						case 'B':
+						case 'b': /*insert function here*/break;
+						case 'C':
+						case 'c': /*insert function here*/break;
+						case 'X':
+						case 'x': buff=false; break;
+						default: break;
+					}
+				} buff=true; break;
+			case '3': 
+				while(buff){
+					system("cls");
+					cout<<"[a] Create Schedule"<<endl;
+					cout<<"[b] View Schedule"<<endl;
+					cout<<"[x] Back"<<endl;
+					cout<<"Enter option: ";
+					cin>>choice;
+					switch(choice){
+						case 'A':
+						case 'a': /*insert function here*/ break;
+						case 'B':
+						case 'b': /*insert function here*/ break;
+						case 'X':
+						case 'x': buff=false; break;
+						default: break;
+					} 
+				} buff = true; break;
+			case '4': /*insert function here*/ break;
+			case '5': exit(0);
+			default: cout<<"Wrong Input. Try again.\n"; break;
+		}
+		cout<<"----\nPress any key to continue.";
+		system("pause>0");
+	}
+	return 0;	
 }
